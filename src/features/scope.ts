dfa721a16cb10bc346895d6c7083182275fd0b5f
libondev <bon.li@outlook.com>
2025-02-06 18:18:22 +0800

    optimize: variable scope select

--- src/features/scope.ts
+++ src/features/scope.ts
@@ -1,15 +1,39 @@
 import type { Position, Selection, TextDocument } from 'vscode'
 
-// const BREAK_CHARACTER = ' \t\n={}()[]'
-const BREAK_CHARACTER = ' \t\n={}'
+const BREAK_CHARACTER = ' \t\n={}()[]'
 
-const IS_SYMBOL_STARTS = /^[}\])?.=]*([\s\S]*?)(?:[{([?.=]*)$/
-const IS_BRACKETS_ENDS_MAP = { '(': ')', '{': '}', '[': ']' }
-const ONLY_SPECIAL_CHARS = /^[^a-zA-Z0-9]+$/
+// only special characters
+// 如果内容不是英文字符/数字/_/$ 的话则返回空字符串
+const ONLY_HAS_SYMBOL_REGEX = /^[^a-zA-Z0-9_$]+$/
 
-const ENLARGED_SELECTION_SYMBOLS = '\'"`'
+const IS_SYMBOL_START_REGEX = /^[}\])?.=;]*([\s\S]*?)(?:[{([?.=;]*)$/
+
+/**
+ * Is member call
+ * 是否是方法/属性调用的符号作为起始位置
+ * @example
+ * ```
+ * .test
+ * ?.test
+ * ::stdout()
+ * ```
+ */
+const IS_MEMBER_CALL = '.:?'
+
+// 成对符号的映射
+const PAIRED_SYMBOL_MAP = {
+  '(': ')',
+  '{': '}',
+  '[': ']',
+  '"': '"',
+  '`': '`',
+  '\'': '\'',
+}
+
+type PairedSymbolKeys = keyof typeof PAIRED_SYMBOL_MAP
 
 // Find the correct character position at the beginning/end of the string
+// 如果成对的符号没有正确闭合的话则继续查找正确的闭合位置
 function ensureCorrectPosition(text: string, start: number, char: string, reverse: boolean) {
   const searchMethod = reverse ? text.lastIndexOf.bind(text) : text.indexOf.bind(text)
 
@@ -25,82 +49,106 @@ function ensureCorrectPosition(text: string, start: number, char: string, revers
 }
 
 // Get the word at the given position
-function getWordAtPosition(document: TextDocument, position: Position): string {
-  const { isEmptyOrWhitespace, text } = document.lineAt(position.line)
+function getCorrectVariableScope(document: TextDocument, anchorPosition: Position): string {
+  const { isEmptyOrWhitespace, text } = document.lineAt(anchorPosition.line)
 
   // empty line or no word
   if (isEmptyOrWhitespace) {
     return ''
   }
 
-  const word = document.getWordRangeAtPosition(position)
+  let startAt = anchorPosition.character
+  let endAt = anchorPosition.character
 
-  let startAt = position.character
-  let endAt = position.character
+  const word = document.getWordRangeAtPosition(anchorPosition)
 
   if (word) {
     startAt = word.start.character
     endAt = word.end.character
   }
 
-  // Until you find the first delimiter
+  // Until the first delimiter is found
+  // 直到找到第一个隔断符
   while (startAt > 0 && !BREAK_CHARACTER.includes(text[startAt - 1])) {
     startAt--
   }
 
-  // e.g.: obj.value?.[0]?.test(  );    or empty text
+  // trim tail semicolon
+  while (text[endAt - 1] === ';') {
+    endAt--
+  }
+
+  let content = text.slice(startAt, endAt)
+
+  // js spread operator
+  if (content.startsWith('...')) {
+    return content.slice(3)
+  }
+
+  // is empty text, or only special characters, or member call
+  // e.g.: obj.value?.[0]?.test(  );
   //                           ^  ^
-  if (startAt === endAt) {
-    // Avoid including truncated spaces, so +1 is required.
+  if (startAt === endAt || IS_MEMBER_CALL.includes(text[startAt])) {
+    // Find the position of the first space from the starting position to the left,
+    // Avoid including truncated spaces, so + 1 is required.
+    // 从开始位置向左查找第一个空格的位置，避免包含截断的空格，所以需要 +1
     const whitespaceIndex = text.lastIndexOf(' ', startAt) + 1
 
-    let newContent = text.slice(whitespaceIndex, endAt)
+    content = text.slice(whitespaceIndex, endAt)
 
-    // If the content is only special characters, return an empty string
-    // e.g.: ('{}','()','[]') => ''
-    if (ONLY_SPECIAL_CHARS.test(newContent)) {
+    // If the content is only special characters, return an empty string, e.g.: '{}', '()', '[]'
+    if (ONLY_HAS_SYMBOL_REGEX.test(content)) {
       return ''
     }
 
-    const lastChar = newContent[newContent.length - 1]
+    const lastChar = content[content.length - 1] as PairedSymbolKeys
 
     // Add brackets if the last character is a bracket. e.g. 'foo(' => 'foo()'
-    if (lastChar in IS_BRACKETS_ENDS_MAP) {
-      newContent += IS_BRACKETS_ENDS_MAP[lastChar as keyof typeof IS_BRACKETS_ENDS_MAP]
+    if (lastChar in PAIRED_SYMBOL_MAP) {
+      content += PAIRED_SYMBOL_MAP[lastChar]
     }
 
-    return newContent.replace(IS_SYMBOL_STARTS, '$1') // e.g.: ?.args | ...args
+    return content.replace(IS_SYMBOL_START_REGEX, '$1')
   }
 
-  // Get the text content of this range
-  const content = text.slice(startAt, endAt)
-
   // only character: '.', ')', ']', etc.
-  if (ONLY_SPECIAL_CHARS.test(content)) {
+  if (ONLY_HAS_SYMBOL_REGEX.test(content)) {
     return ''
   }
 
-  const _firstChar = content[0]
-  const _lastChar = content[content.length - 1]
+  /**
+   * get the start and end key paired symbol
+   * 获取成对符号的开始和结束符号
+   * @example
+   * ```js
+   * 'lorem -> 'lorem'
+   * "lorem -> "lorem"
+   * `lorem -> `lorem`
+   * [lorem -> [lorem]
+   * (lorem -> (lorem)
+   * ```
+   */
+  const startPairedSymbol = PAIRED_SYMBOL_MAP[content[0] as PairedSymbolKeys]
+  const endPairedSymbol = PAIRED_SYMBOL_MAP[content[content.length - 1] as PairedSymbolKeys]
 
-  // completion string symbol. ( 'lorem   ->  'lorem' )
-  // if the first character is a symbol, then we need to enlarge the selection
-  if (ENLARGED_SELECTION_SYMBOLS.includes(_firstChar)) {
-    endAt = ensureCorrectPosition(text, startAt, _firstChar, false)
+  if (startPairedSymbol && !endPairedSymbol) {
+    endAt = ensureCorrectPosition(text, startAt, startPairedSymbol, false)
 
     return text.slice(startAt, endAt)
-  } else if (ENLARGED_SELECTION_SYMBOLS.includes(_lastChar)) {
-    startAt = ensureCorrectPosition(text, startAt, _lastChar, true)
+  } else if (!startPairedSymbol && endPairedSymbol) {
+    startAt = ensureCorrectPosition(text, startAt, endPairedSymbol, true)
 
     return text.slice(startAt, endAt)
   }
 
-  return content.replace(IS_SYMBOL_STARTS, '$1') // e.g.: ?.args | ...args
+  return content
 }
 
 export function getScope(document: TextDocument, selection: Selection): string {
+  // If the selection of the cursor is empty,
+  // the selection is obtained in a form conforming to the programming grammar.
   if (selection.isEmpty) {
-    return getWordAtPosition(document, selection.anchor)
+    return getCorrectVariableScope(document, selection.anchor)
   }
 
   const { isEmptyOrWhitespace, text } = document.lineAt(selection.start.line)

984049ea12110ee08a6faeccd83cbff22e45e67d
libondev <bon.li@outlook.com>
2025-08-13 16:05:42 +0800

    feat: support object/array deconstruction assignment

--- playground/javascript.js
+++ playground/javascript.js
@@ -83,7 +83,7 @@ function initApp(Vue) {
     router,
     store,
     i18n,
-    render: () => h(App),
+    render: () => h(App)
   })
 
   const arr = new Array([
@@ -114,11 +114,29 @@ if (Math.random() > 0.5) {
 
   )
 
-} else {
-  initApp(
+}
 
-    obj['name'].length,
+export function getTemplate(colors) {
+  const {
+    name,
+    mode,
+    accent,
+    border,
+    background,
+    editorForeground,
+    foreground,
+    ...theme
+  } = colors
 
-  )
+  theme.key ||= theme.css
+  theme.css ||= theme.variable
+  theme.json ||= theme.property
+  theme.regexp ||= theme.string
+  theme.storage ||= theme.keyword
+  theme.classes ||= theme.attribute
+  theme.inputBackground ||= background
+  theme.sidebarBackground ||= background
+  theme.activeBackground ||= theme.sidebarBackground
 
+  return theme
 }
--- src/utils/block-scope.ts
+++ src/utils/block-scope.ts
@@ -41,6 +41,8 @@ const blockEndSymbolRegexp = generateBlockRegexp([
   '};?',
   '];?',
   ');?',
+  '} =.*;?',
+  '] =.*;?',
   ' as .*;?',
   ' satisfies .*;?',
 ])
